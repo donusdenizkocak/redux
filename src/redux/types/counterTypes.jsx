271568996bc8fdeadb271c2aa75366027de5a403
denizderK <denizderya8668@gmail.com>
2023-05-27 21:39:56 +0300

    edt

--- src/redux/types/counterTypes.jsx
+++ src/redux/types/counterTypes.jsx
@@ -1,4 +1,4 @@
-export const INCREASE="INCREASE"
-export const DECREASE = 'DECREASE'
+export const INCRIMENT="INCRIMENT"
+export const DECREMENT = 'DECREMENT'
 export const RESET = 'RESET'
 //! stringi degişken haline getirdik   (rxconst sinippeti kullan)
